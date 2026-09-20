ae9d2d4a4ae6bbde8c6bdf3eefbb7bb8857bb0bf
Saptarshi Pal <av.en.u4aid25063@av.students.amrita.edu>
2026-09-20 23:39:51 +0530

    putting confidential info in .env

--- app/database.py
+++ app/database.py
@@ -3,9 +3,10 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import psycopg2
 from psycopg2.extras import RealDictCursor
+from .config import settings
 
-# SQLALCHEMY_DATABASE_URL= 'postgresql://<username>:<password>@<ip-address/hostname>/<databse_name>'
-SQLALCHEMY_DATABASE_URL='postgresql://postgres:1234@localhost/social media'
+# SQLALCHEMY_DATABASE_URL= 'postgresql://<username>:<password>@<ip-address/hostname>:<database_port>/<databse_name>'
+SQLALCHEMY_DATABASE_URL=f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 engine=create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal=sessionmaker(autoflush=False,autocommit=False,bind=engine) #responsible to talk to our databases
 base=declarative_base()
